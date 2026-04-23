d9c0f379907babe538d5b8391aafef7b43f6490b
diegosamuel132630-ship-it <diegosamuel132630@gmail.com>
2026-04-23 15:07:56 +0100

    Delete jogo.py

--- jogo.py
+++
@@ -1,5 +0,0 @@
-# ==========================================
-# 🎮 JOGO GESTOR DE GUERRA AAA (ORGANIZADO POR SISTEMAS + MAIN CENTRAL)
-# ==========================================
-
-FICHEIRO = "dados_jogo.json"
