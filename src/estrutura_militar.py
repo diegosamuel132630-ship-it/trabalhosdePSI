bbe2f9b73c9251783b2e961cc957264d0f0ea1bc
diegosamuel132630-ship-it <diegosamuel132630@gmail.com>
2026-05-14 15:24:25 +0100

    Update estrutura_militar.py

--- src/estrutura_militar.py
+++ src/estrutura_militar.py
@@ -1,66 +1,8 @@
-estruturas = []
-next_id = 1
-
-
-def criar(nome, valor, resp, pais):
-    global next_id
-
-    e = {
-        "id": next_id,
-        "nome": nome,
-        "valor": valor,
-        "responsavel": resp,
-        "pais": pais
-    }
-
-    estruturas.append(e)
-    next_id += 1
-    return 201, e
-
-
-def listar():
-    if not estruturas:
-        return 404, "Sem estruturas"
-    return 200, estruturas
-
-
-def buscar(id_):
-    for e in estruturas:
-        if e["id"] == id_:
-            return 200, e
-    return 404, "Não encontrado"
-
-
-def atualizar(id_, nome, valor, resp, pais):
-    for e in estruturas:
-        if e["id"] == id_:
-            e.update({
-                "nome": nome,
-                "valor": valor,
-                "responsavel": resp,
-                "pais": pais
-            })
-            return 200, e
-
-    return 404, "Não encontrado"
-
-
-def apagar(id_):
-    for e in estruturas:
-        if e["id"] == id_:
-            estruturas.remove(e)
-            return 200, e
-
-    return 404, "Não encontrado"
-
+import json
 
 # =========================
 #  PERSISTÊNCIA (ADICIONADO)
 # =========================
-
-import json
-
-
 def guardar_dados(nome_ficheiro="estruturas.json"):
     try:
         with open(nome_ficheiro, "w", encoding="utf-8") as f:
@@ -92,3 +34,68 @@ def carregar_dados(nome_ficheiro="estruturas.json"):
 
     except Exception as e:
         return 500, str(e)
+
+
+estruturas = []
+next_id = 1
+
+
+def criar(nome, valor, resp, pais):
+    global next_id
+    carregar_dados()
+
+    e = {
+        "id": next_id,
+        "nome": nome,
+        "valor": valor,
+        "responsavel": resp,
+        "pais": pais
+    }
+
+    estruturas.append(e)
+    next_id += 1
+    guardar_dados()
+    return 201, e
+
+
+def listar():
+    carregar_dados()
+    if not estruturas:
+        return 404, "Sem estruturas"
+    return 200, estruturas
+
+
+def buscar(id_):
+    carregar_dados()
+    for e in estruturas:
+        if e["id"] == id_:
+            return 200, e
+    return 404, "Não encontrado"
+
+
+def atualizar(id_, nome, valor, resp, pais):
+    carregar_dados()
+    for e in estruturas:
+        if e["id"] == id_:
+            e.update({
+                "nome": nome,
+                "valor": valor,
+                "responsavel": resp,
+                "pais": pais
+            })
+            guardar_dados()
+            return 200, e
+
+    return 404, "Não encontrado"
+
+
+def apagar(id_):
+    carregar_dados()
+    for e in estruturas:
+        if e["id"] == id_:
+            estruturas.remove(e)
+            guardar_dados()
+            return 200, e
+
+    return 404, "Não encontrado"
+
