a325151a1ec0dd8b5250afbedd89a2ddc2ac9ab5
diegosamuel132630-ship-it <diegosamuel132630@gmail.com>
2026-05-14 15:26:06 +0100

    Update ministerio_defesa.py

--- src/ministerio_defesa.py
+++ src/ministerio_defesa.py
@@ -1,65 +1,13 @@
+import json
 from soldados import soldados
 
 missoes = []
 next_id = 1
 
 
-def criar(nome, pais, ids_soldados):
-    global next_id
-
-    missao = {
-        "id": next_id,
-        "nome": nome,
-        "pais": pais,
-        "soldados": ids_soldados
-    }
-
-    missoes.append(missao)
-    next_id += 1
-    return 201, missao
-
-
-def listar():
-    if not missoes:
-        return 404, "Sem missões"
-    return 200, missoes
-
-
-def buscar(id_):
-    for m in missoes:
-        if m["id"] == id_:
-            return 200, m
-    return 404, "Não encontrado"
-
-
-def atualizar(id_, nome, pais, ids_soldados):
-    for m in missoes:
-        if m["id"] == id_:
-            m.update({
-                "nome": nome,
-                "pais": pais,
-                "soldados": ids_soldados
-            })
-            return 200, m
-
-    return 404, "Não encontrado"
-
-
-def apagar(id_):
-    for m in missoes:
-        if m["id"] == id_:
-            missoes.remove(m)
-            return 200, m
-
-    return 404, "Não encontrado"
-
-
 # =========================
-#  PERSISTÊNCIA (ADICIONADO)
+# PERSISTÊNCIA
 # =========================
-
-import json
-
 
 def guardar_dados(nome_ficheiro="missoes.json"):
     try:
@@ -92,3 +40,73 @@ def carregar_dados(nome_ficheiro="missoes.json"):
 
     except Exception as e:
         return 500, str(e)
+
+def criar(nome, pais, ids_soldados):
+    global next_id
+
+    carregar_dados()
+
+    missao = {
+        "id": next_id,
+        "nome": nome,
+        "pais": pais,
+        "soldados": ids_soldados
+    }
+
+    missoes.append(missao)
+
+    next_id += 1
+    guardar_dados()
+
+    return 201, missao
+
+
+def listar():
+    carregar_dados()
+
+    if not missoes:
+        return 404, "Sem missões"
+
+    return 200, missoes
+
+
+def buscar(id_):
+    carregar_dados()
+
+    for m in missoes:
+        if m["id"] == id_:
+            return 200, m
+
+    return 404, "Não encontrado"
+
+
+def atualizar(id_, nome, pais, ids_soldados):
+    carregar_dados()
+
+    for m in missoes:
+        if m["id"] == id_:
+
+            m.update({
+                "nome": nome,
+                "pais": pais,
+                "soldados": ids_soldados
+            })
+
+            guardar_dados()
+            return 200, m
+
+    return 404, "Não encontrado"
+
+
+def apagar(id_):
+    carregar_dados()
+
+    for m in missoes:
+        if m["id"] == id_:
+
+            missoes.remove(m)
+
+            guardar_dados()
+            return 200, m
+
+    return 404, "Não encontrado"
