d738d5fdffeff7319abc1bbd06848bb1a3a04a99
diegosamuel132630-ship-it <diegosamuel132630@gmail.com>
2026-05-14 15:27:07 +0100

    Update soldados.py

--- src/soldados.py
+++ src/soldados.py
@@ -1,65 +1,12 @@
+import json
+
 soldados = []
 next_id = 1
 
 
-def criar(nome, idade, patente, pais):
-    global next_id
-
-    s = {
-        "id": next_id,
-        "nome": nome,
-        "idade": idade,
-        "patente": patente,
-        "pais": pais
-    }
-
-    soldados.append(s)
-    next_id += 1
-    return 201, s
-
-
-def listar():
-    if not soldados:
-        return 404, "Sem soldados"
-    return 200, soldados
-
-
-def buscar(id_):
-    for s in soldados:
-        if s["id"] == id_:
-            return 200, s
-    return 404, "Não encontrado"
-
-
-def atualizar(id_, nome, idade, patente, pais):
-    for s in soldados:
-        if s["id"] == id_:
-            s.update({
-                "nome": nome,
-                "idade": idade,
-                "patente": patente,
-                "pais": pais
-            })
-            return 200, s
-
-    return 404, "Não encontrado"
-
-
-def apagar(id_):
-    for s in soldados:
-        if s["id"] == id_:
-            soldados.remove(s)
-            return 200, s
-
-    return 404, "Não encontrado"
-
-
 # =========================
-#  PERSISTÊNCIA (ADICIONADO)
+# PERSISTÊNCIA
 # =========================
-
-import json
-
 
 def guardar_dados(nome_ficheiro="soldados.json"):
     try:
@@ -92,3 +39,78 @@ def carregar_dados(nome_ficheiro="soldados.json"):
 
     except Exception as e:
         return 500, str(e)
+
+def criar(nome, idade, patente, pais):
+    global next_id
+
+    carregar_dados()
+
+    s = {
+        "id": next_id,
+        "nome": nome,
+        "idade": idade,
+        "patente": patente,
+        "pais": pais
+    }
+
+    soldados.append(s)
+
+    next_id += 1
+
+    guardar_dados()
+
+    return 201, s
+
+
+def listar():
+    carregar_dados()
+
+    if not soldados:
+        return 404, "Sem soldados"
+
+    return 200, soldados
+
+
+def buscar(id_):
+    carregar_dados()
+
+    for s in soldados:
+        if s["id"] == id_:
+            return 200, s
+
+    return 404, "Não encontrado"
+
+
+def atualizar(id_, nome, idade, patente, pais):
+    carregar_dados()
+
+    for s in soldados:
+        if s["id"] == id_:
+
+            s.update({
+                "nome": nome,
+                "idade": idade,
+                "patente": patente,
+                "pais": pais
+            })
+
+            guardar_dados()
+
+            return 200, s
+
+    return 404, "Não encontrado"
+
+
+def apagar(id_):
+    carregar_dados()
+
+    for s in soldados:
+        if s["id"] == id_:
+
+            soldados.remove(s)
+
+            guardar_dados()
+
+            return 200, s
+
+    return 404, "Não encontrado"
